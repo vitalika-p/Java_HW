c346e1fed906bfa3d0956cf274658dcf48880c07
Vitalika <vitamin@MacBook-Air-Vitalika.local>
2024-10-08 00:07:11 +0300

    added getMessage

--- qa_guru_31_git/src/main/java/qaguru/java/Main.java
+++ qa_guru_31_git/src/main/java/qaguru/java/Main.java
@@ -2,6 +2,10 @@ package qaguru.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage () {
+        return "Hello qa.guru";
     }
 }
